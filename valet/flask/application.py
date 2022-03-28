83caf39f9813d5e8230f2c1f11ccdaacda6ca349
kemo-byte <kamalkafi12@gmail.com>
2022-03-28 06:26:09 +0200

    random number library

--- valet/flask/application.py
+++ valet/flask/application.py
@@ -1,11 +1,14 @@
+import random;
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+number=random.randint(1,10)
 @app.route("/")
 
 def index():
-  return render_template('index.html',name='kamal')
+  return render_template('index.html',number=number)
   
 @app.route('/goodbye')
 def bye():
